d82ba7ebae563c62a2572314a8860181a9ba59f3
Tanisha <tanishavirgo.26@gmail.com>
2021-06-03 22:44:05 +0530

    programs

---
+++ setMatrixZeroes.java
@@ -0,0 +1,18 @@
+public class setMatrixZeroes {
+    public void setZeroes(int[][] matrix) {
+        int cols=1,rows=matrix.length,columns=matrix[0].length;
+         for(int i=0;i<rows;i++){
+             if(matrix[i][0] == 0)cols=0;
+           for(int j=1;j<columns;j++)
+               if(matrix[i][j]==0)
+                   matrix[i][0]=matrix[0][j]=0;
+           } 
+             for(int i=rows-1;i>=0;i--){
+                 for(int j = columns-1;j>=1;j--)
+                     if(matrix[i][0]==0 || matrix[0][j]==0)
+                         matrix[i][j]=0;
+                 if(cols == 0)
+                     matrix[i][0]=0;
+             }
+         }
+}
