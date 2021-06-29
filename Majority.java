1948f68b95d044e921fb3127cc72d102c7449004
Tanisha <tanishavirgo.26@gmail.com>
2021-06-30 01:55:42 +0530

    programs

---
+++ Majority.java
@@ -0,0 +1,22 @@
+public class Majority {
+    public int majorityElement(int[] nums) {
+        int count =0;
+        int candidate=0;
+        for(int num:nums)
+        {
+            if(count==0)
+            {
+                candidate=num;
+            }
+            if(num==candidate)
+            {
+                count= count+1;
+            }
+            else
+            {
+                count=count-1;
+           }
+        }  
+        return candidate;
+    } 
+}
